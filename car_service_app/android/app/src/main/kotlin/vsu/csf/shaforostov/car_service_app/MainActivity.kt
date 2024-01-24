759ba7e942951ba1017e4f9830d8a12232286d1e
MiSticksman <vadim02101@gmail.com>
2024-01-25 02:28:39 +0300

    init mobile app

---
+++ car_service_app/android/app/src/main/kotlin/vsu/csf/shaforostov/car_service_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package vsu.csf.shaforostov.car_service_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ employee_app/android/app/src/main/kotlin/vsu/csf/shaforostov/employee_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package vsu.csf.shaforostov.employee_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
